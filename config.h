638932b4a627e7a4a8501a87958c5dfcf281013b
Emre AKYUZ <emreakyuz_2@hotmail.com>
2022-10-23 03:46:15 +0000

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("sb-music",    0,  11),
+	BLOCK("sb-news",     0,  6),
+	BLOCK("sb-mailbox",  0,  12),
+	BLOCK("sb-volume",   0,  10),
+	BLOCK("sb-clock",    60, 1),
+};
+
